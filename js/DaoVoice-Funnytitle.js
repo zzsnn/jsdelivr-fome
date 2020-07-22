0fa07c4aea628374492a8eff34945c8cad2972cc
TheFine <768463945@qq.com>
2020-07-23 05:40:21 +0800

    更新

--- js/DaoVoice-Funnytitle.js
+++ js/DaoVoice-Funnytitle.js
@@ -1,12 +1,20 @@
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
-    "menus_item_child"
-);
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " menus-closed";
-}
+// 评论 //
+(function (i, s, o, g, r, a, m) {
+    i["DaoVoiceObject"] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    a.charset = "utf-8";
+    m.parentNode.insertBefore(a, m)
+})(window, document, "script", ('https:' == document.location.protocol ? 'https:' : 'http:') + "//cdn.jsdelivr.net/gh/zzsnn/jsdelivr-fome/js/bundle.b69d69b9cd164a70039e.js", "daovoice");
+daovoice('init', {
+    app_id: "fd4499fa"
+});
+daovoice('update');
+
 // 搞怪标题 //
  var OriginTitle = document.title;
  var titleTime;
@@ -24,7 +32,17 @@ for (var i = 0; i < menus_item_child.length; i++) {
          }, 2000);
      }
  });
-// 日历 //
+//手机侧边折叠//
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+  "menus_item_child"
+);
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+  menus_item_child[i].style.display = "none";
+}
+
+//日历//
 (function($) {
   var aCalendar = function(language, options, object) {
     var now = new Date();
@@ -913,19 +931,3 @@ var calLanguages = {
     dayOfWeek: ['კვირა', 'ორშაბათი', 'სამშაბათი', 'ოთხშაბათი', 'ხუთშაბათი', 'პარასკევი', 'შაბათი']
   },
 };
-// 评论 //
-(function (i, s, o, g, r, a, m) {
-    i["DaoVoiceObject"] = r;
-    i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    a.charset = "utf-8";
-    m.parentNode.insertBefore(a, m)
-})(window, document, "script", ('https:' == document.location.protocol ? 'https:' : 'http:') + "//cdn.jsdelivr.net/gh/zzsnn/jsdelivr-fome/js/bundle.b69d69b9cd164a70039e.js", "daovoice");
-daovoice('init', {
-    app_id: "fd4499fa"
-});
-daovoice('update');
